5cb706338b35a846aa07555338c058165ce906d2
Ayushi Agrawal <78734857+ayushi-sxg@users.noreply.github.com>
2021-07-10 11:33:06 +0530

    weather updates

---
+++ config.js
@@ -0,0 +1,9 @@
+const constants= {
+    openWeatherMap:{
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "451f5beb72fb4bc949c816b8dddf40c3"
+    }
+}
+
+
+module.exports= constants;
